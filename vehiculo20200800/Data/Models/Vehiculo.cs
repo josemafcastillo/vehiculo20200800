eef58ef64edd063a245f940921d781084c36f460
josemafcastillo <117459252+josemafcastillo@users.noreply.github.com>
2022-11-04 10:45:28 -0400

    Add files via upload

---
+++ vehiculo20200800/Data/Models/Vehiculo.cs
@@ -0,0 +1,9 @@
+namespace vehiculo20200800.Data.Models;
+
+public class Vehiculos{
+    public int VehiculoID { get; set; }
+    public int Marca { get; set; }
+    public int Modelo { get; set; }
+    public int Año { get; set; }
+    public int Color { get; set; }
+}
